3de764c72ce0576b58f588256df19d3312065bd6
diego <mauro3333@gmail.com>
2023-03-12 22:41:13 -0700

    final commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,17 @@
+import "./Header.css";
+
 export const Header = () => {
-    return(
-        <div>
-            <button>Home</button>
-            <button>Contact</button>
-            <button>Register</button>
-        </div>
-    )
-}
+	return (
+		<header>
+			<div className="title">
+				<h1>REACT - FIRST ASSIGNMENT</h1>
+			</div>
+
+			<div className="buttons">
+				<button>Home</button>
+				<button>Contact</button>
+				<button>Register</button>
+			</div>
+		</header>
+	);
+};
